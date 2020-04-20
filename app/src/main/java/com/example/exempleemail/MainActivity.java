0501261be8dbd7a6cccbfa056adf65593442622e
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-20 10:44:27 +0200

    clean up

--- app/src/main/java/com/example/exempleemail/MainActivity.java
+++ app/src/main/java/com/example/exempleemail/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
